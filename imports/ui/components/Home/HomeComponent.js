bde2f950d0802ef137064a07754ae76f629e1aa9
nsakapeter <nsakapeter@gmail.com>
2019-02-12 07:48:42 -0700

    Multiple Disease simulation UI

--- imports/ui/components/Home/HomeComponent.js
+++ imports/ui/components/Home/HomeComponent.js
@@ -14,31 +14,73 @@ Template.Home.helpers({
   simulateMultiple () {
     return Template.instance().simulateMultiple.get();
   },
+  universalParameters () {
+    return Template.instance().universalParameters.get();
+  },
+  simulateMultipleResults () {
+    return Template.instance().simulateMultipleResults.get();
+  },
+
+
 });
 
 Template.Home.events({
   // 'click .simulateMultipleLabel': function(e, instance) {
     'click .simulateMultipleLabel'(event, instance) {
-    alert("hi");
-    // var event_instance = instance;
-    // var el = $('.simulateMultipleCheckbox');
-    // if (el.is(':checked') == true){
-    //   event_instance.simulateMultiple.set(false);
-    //   alert("hi");
-    // }
-    // else{
-    //   event_instance.simulateMultiple.set(true);
-    //     alert("hey");
-    // }
+      // event.preventDefault();
+      // event.stopPropagation();
+        alert("hi");
+
+    var event_instance = instance;
+    var el = $('.simulateMultipleCheckbox');
+    if (el.is(':checked') == true){
+      event_instance.simulateMultiple.set(false);
+      alert("hi");
+    }
+    else{
+      event_instance.simulateMultiple.set(true);
+        alert("hey");
+    }
 
 
   },
+  'click .universalParametersLabel'(event, instance) {
+    // event.preventDefault();
+    // event.stopPropagation();
+      alert("hi");
+
+  var event_instance = instance;
+  var el = $('.unversalParametersCheckbox');
+  if (el.is(':checked') == true){
+    event_instance.universalParameters.set(false);
+    alert("hi");
+  }
+  else{
+    event_instance.universalParameters.set(true);
+      alert("hey");
+  }
+
+
+},
+'click .simulateMultipleResults'(event, instance) {
+  event.preventDefault();
+  event.stopPropagation();
+  instance.simulateMultipleResults.set(true);
+},
+'click .simulateMultipleEdit'(event, instance) {
+  event.preventDefault();
+  event.stopPropagation();
+  instance.simulateMultipleResults.set(false);
+},
 });
 
 Template.Home.onCreated(function() {
   var instance;
   instance = this;
   instance.simulateMultiple = new ReactiveVar(true);
+  instance.universalParameters = new ReactiveVar(true);
+  instance.simulateMultipleResults = new ReactiveVar(false);
+
 });
 
 Template.Home.onRendered(function() {
