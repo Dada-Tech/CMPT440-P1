d433795ad2d43164ba58a965867a224fd52b5407
Volkan Yasa <volkan1209@gmail.com>
2019-02-15 14:44:52 -0700

    Test update

--- imports/ui/components/Graph/Graph.js
+++ imports/ui/components/Graph/Graph.js
@@ -2,9 +2,9 @@ import { Template } from 'meteor/templating'
 import { Meteor } from 'meteor/meteor'
 
 Template.Graph.helpers({
-  // simulateMultiple () {
-  //   return Template.instance().simulateMultiple.get();
-  // }
+  modelName () {
+    return Template.instance().modelName.get();
+  }
 
 
 });
@@ -17,18 +17,25 @@ Template.Graph.events({
 });
 
 Template.Graph.onCreated(function() {
+  instance.modelName = new ReactiveVar("SIRS");
+
+  instance.modelName.set("SIR");
   var instance;
   instance = this;
   alert("Hi");
   var susceptible_population = 3;
   var infected_population = 4;
   var recovered_population = 5;
+  var total_population = susceptible_population + infected_population + recovered_population;
   var beta = 5;
   var gamma = 6;
   var birth_rate = 0;
   var death_rate = 0;
   var days = 50;
-  // instance.simulateMultiple = new ReactiveVar(false);
+
+
+
+  
 
 
 });
